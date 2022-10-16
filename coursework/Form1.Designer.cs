aee06f712b54ae3ebc8d7ee768e157618e76cd66
Jakubyte <jakubdymar@gmail.com>
2022-10-16 18:56:36 +0100

    Add project files.

---
+++ coursework/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace coursework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Favourites");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("History");
+            this.searchbar = new System.Windows.Forms.TextBox();
+            this.searchbarButton = new System.Windows.Forms.Button();
+            this.OutputDataBox = new System.Windows.Forms.TextBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.ssStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.bookmarks = new System.Windows.Forms.TreeView();
+            this.btnBookmarsAddFavourite = new System.Windows.Forms.Button();
+            this.btnBookmarksDelete = new System.Windows.Forms.Button();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // searchbar
+            // 
+            this.searchbar.Location = new System.Drawing.Point(18, 18);
+            this.searchbar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.searchbar.Name = "searchbar";
+            this.searchbar.Size = new System.Drawing.Size(1166, 26);
+            this.searchbar.TabIndex = 0;
+            this.searchbar.TextChanged += new System.EventHandler(this.Searchbar_TextChanged);
+            // 
+            // searchbarButton
+            // 
+            this.searchbarButton.Location = new System.Drawing.Point(1196, 17);
+            this.searchbarButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.searchbarButton.Name = "searchbarButton";
+            this.searchbarButton.Size = new System.Drawing.Size(112, 32);
+            this.searchbarButton.TabIndex = 2;
+            this.searchbarButton.Text = "search";
+            this.searchbarButton.UseVisualStyleBackColor = true;
+            this.searchbarButton.Click += new System.EventHandler(this.SearchbarButton_Click);
+            // 
+            // OutputDataBox
+            // 
+            this.OutputDataBox.AcceptsReturn = true;
+            this.OutputDataBox.AcceptsTab = true;
+            this.OutputDataBox.Location = new System.Drawing.Point(285, 52);
+            this.OutputDataBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.OutputDataBox.Multiline = true;
+            this.OutputDataBox.Name = "OutputDataBox";
+            this.OutputDataBox.ReadOnly = true;
+            this.OutputDataBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.OutputDataBox.Size = new System.Drawing.Size(1023, 687);
+            this.OutputDataBox.TabIndex = 3;
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ssStatus});
+            this.statusStrip.Location = new System.Drawing.Point(0, 1139);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Padding = new System.Windows.Forms.Padding(2, 0, 21, 0);
+            this.statusStrip.Size = new System.Drawing.Size(1326, 32);
+            this.statusStrip.TabIndex = 4;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // ssStatus
+            // 
+            this.ssStatus.Name = "ssStatus";
+            this.ssStatus.Size = new System.Drawing.Size(81, 25);
+            this.ssStatus.Text = "200 (OK)";
+            // 
+            // bookmarks
+            // 
+            this.bookmarks.Location = new System.Drawing.Point(18, 52);
+            this.bookmarks.Name = "bookmarks";
+            treeNode3.Name = "";
+            treeNode3.Text = "Favourites";
+            treeNode4.Name = "";
+            treeNode4.Text = "History";
+            this.bookmarks.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode4});
+            this.bookmarks.Size = new System.Drawing.Size(249, 498);
+            this.bookmarks.TabIndex = 5;
+            this.bookmarks.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.bookmarks_AfterSelect);
+            this.bookmarks.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.bookmarks_NodeMouseDoubleClick);
+            // 
+            // btnBookmarsAddFavourite
+            // 
+            this.btnBookmarsAddFavourite.Location = new System.Drawing.Point(18, 556);
+            this.btnBookmarsAddFavourite.Name = "btnBookmarsAddFavourite";
+            this.btnBookmarsAddFavourite.Size = new System.Drawing.Size(75, 33);
+            this.btnBookmarsAddFavourite.TabIndex = 6;
+            this.btnBookmarsAddFavourite.Text = "ATF";
+            this.btnBookmarsAddFavourite.UseVisualStyleBackColor = true;
+            this.btnBookmarsAddFavourite.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnBookmarsAddFavourite_MouseUp);
+            // 
+            // btnBookmarksDelete
+            // 
+            this.btnBookmarksDelete.Location = new System.Drawing.Point(100, 556);
+            this.btnBookmarksDelete.Name = "btnBookmarksDelete";
+            this.btnBookmarksDelete.Size = new System.Drawing.Size(75, 32);
+            this.btnBookmarksDelete.TabIndex = 7;
+            this.btnBookmarksDelete.Text = "Del";
+            this.btnBookmarksDelete.UseVisualStyleBackColor = true;
+            this.btnBookmarksDelete.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnBookmarksDelete_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1326, 1171);
+            this.Controls.Add(this.btnBookmarksDelete);
+            this.Controls.Add(this.btnBookmarsAddFavourite);
+            this.Controls.Add(this.bookmarks);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.OutputDataBox);
+            this.Controls.Add(this.searchbarButton);
+            this.Controls.Add(this.searchbar);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox searchbar;
+        private System.Windows.Forms.Button searchbarButton;
+        private System.Windows.Forms.TextBox OutputDataBox;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel ssStatus;
+        private System.Windows.Forms.TreeView bookmarks;
+        private System.Windows.Forms.Button btnBookmarsAddFavourite;
+        private System.Windows.Forms.Button btnBookmarksDelete;
+    }
+}
+
---
+++ coursework/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// added libs
+using System.Text.RegularExpressions;
+using System.Net;   // WebRequest, WebResponse
+using System.IO;    // StreamReader
+
+namespace coursework
+{
+    public partial class Form1 : Form
+    {
+        private const string URLPattern = @"((https ?:\/\/)?w{3}\.(\w+[.-]*)+\.(com|co\.uk))";
+        private const string HTTPPattern = @"(https?)";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Searchbar_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SearchbarButton_Click(object sender, EventArgs e)
+        {
+            handleSearch();
+            handleBookmarks(searchbar.Text);
+        }
+
+        private void handleSearch()
+        {
+            string url = searchbar.Text;
+
+            HttpWebResponse res = null;
+
+            try
+            {
+                WebRequest req = WebRequest.Create(url);
+                req.Method = "GET";
+                res = (HttpWebResponse)req.GetResponse();
+                StreamReader data = new StreamReader(res.GetResponseStream());
+                setssStatus("200 (OK)", Color.Green);
+
+                OutputDataBox.Text = data.ReadToEnd();
+            }
+            catch (WebException e)
+            {
+                Regex r = new Regex(@"(\(\d+\).+)");
+                //OutputDataLabel.Text = e.Message;
+
+                MatchCollection errStatusCode = r.Matches(e.Message);
+
+                ssStatus.Text = errStatusCode[0].Value;
+                ssStatus.ForeColor = Color.Red;
+
+                OutputDataBox.Text = "ERR";
+            }
+        }
+
+        private void setssStatus(string s, Color c)
+        {
+            ssStatus.Text = s;
+            ssStatus.ForeColor = c;
+        }
+
+        private void handleBookmarks(string s, int index = 1)
+        {
+            bookmarks.Nodes[index].Nodes.Add(s);
+        }
+
+        private void bookmarks_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            
+        }
+
+        private void bookmarks_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (bookmarks.SelectedNode.Text.Substring(0, 4).Equals("http"))
+            {
+                searchbar.Text = bookmarks.SelectedNode.Text;
+                handleSearch();
+            }
+        }
+
+        private void btnBookmarsAddFavourite_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (searchbar.Text.Equals(""))
+            {
+                return;
+            }
+
+            handleBookmarks(searchbar.Text, 0);
+        }
+
+        private void btnBookmarksDelete_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (bookmarks.SelectedNode == null || bookmarks.SelectedNode == bookmarks.Nodes[0] || bookmarks.SelectedNode == bookmarks.Nodes[1])
+            {
+                return;
+            }
+
+            bookmarks.SelectedNode.Remove();
+        }
+    }
+}
